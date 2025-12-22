ee02e711983a5ffeabaf9d13f40f4adee861501b
bekkilou <bekki.leaver@gmail.com>
2025-12-22 10:42:26 +0000

    Add participant routing

--- app/routes.js
+++ app/routes.js
@@ -7,18 +7,21 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
-// Run this code when a form is submitted to 'juggling-balls-answer'
+// Route through CTIMP & Participants
 router.post('/my-research/project-scope-03', function (req, res) {
 
   // Make a variable and give it the value from 'how-many-balls'
-  var isctimp = req.session.data['researchActivities']
+  var researchActivities = req.session.data['researchActivities']
+  var participantGroups = req.session.data['participantGroups']
 
   // Check whether the variable matches a condition
-  if (isctimp == "Treatment, such as medicines, devices, surgery, vaccines or therapies"){
+  if (researchActivities == "Treatment, such as medicines, devices, surgery, vaccines or therapies"){
     // Send user to next page
     res.redirect('/my-research/project-scope-ctimp')
-  } else {
+  } if (participantGroups == "People who are known to be deceased prior to their inclusion in the project"){
     // Send user to ineligible page
+    res.redirect('/my-research/project-scope-03')
+  } else {
     res.redirect('/my-research/project-scope-participants')
   }
 
