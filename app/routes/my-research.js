a68d76d16069ece720a2ad5a41aad39f13b3b6e9
bekkilou <bekki.leaver@gmail.com>
2026-02-26 13:22:09 +0000

    Update my-research.js

--- app/routes/my-research.js
+++ app/routes/my-research.js
@@ -25,7 +25,7 @@ router.post('/my-research/create-project-check', function (req, res) {
       String(now.getDate()).padStart(2, '0')
 
     const randomPart =
-      Math.random().toString()
+      Math.random().toString(D6)
 
     req.session.data.projectReference = `${randomPart}`
   }
