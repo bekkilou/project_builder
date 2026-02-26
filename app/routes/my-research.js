8beb9cc712e0b3e96845b3f4f132f6976c8b752c
bekkilou <bekki.leaver@gmail.com>
2026-02-26 13:29:11 +0000

    Update my-research.js

--- app/routes/my-research.js
+++ app/routes/my-research.js
@@ -19,15 +19,6 @@ router.post('/my-research/create-project-check', function (req, res) {
       now.toLocaleString('en-GB', { month: 'long' }) + ' ' +
       now.getFullYear()
 
-    const datePart =
-      now.getFullYear().toString().slice(-2) +
-      String(now.getMonth() + 1).padStart(2, '0') +
-      String(now.getDate()).padStart(2, '0')
-
-    const randomPart =
-      Math.random().toString().ToUpper().Substring(0, 6)
-
-    req.session.data.projectReference = `${randomPart}`
   }
 
   res.redirect('/my-research-w-record')
