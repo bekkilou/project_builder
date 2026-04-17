46b46dba9d2414c8074816c372a434e4ada7d12a
bekkilou <bekki.leaver@gmail.com>
2026-04-17 07:58:54 +0100

    Adding some pathways thoughts/experimentation

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,31 @@
 module.exports = {
+  // Text inputs
+  "projectTitle": "My Research Project",
+  "fullProjectTitle": "The Full Title of My Research Project",
 
-  // Insert values here
+  // Checkboxes (arrays)
+  "UKNations": ["England", "Wales"],
+  "participantGroups": ["nhs_patients_service_users", "nhs_hsc_staff"],
+  "researchActivities": ["clinical_people_activities", "treatment", "non_clinical_staff_activities"],
+  "adultsAndChildren": ["adults"],
+  "adultAge": ["18-64"],
+  "hmppsNations": ["England"],
 
+  // Radios (single string values)
+  "nhsHscOrg": "yes",
+  "isCTIMP": "yes",
+  "ctimpCombined": "combined",
+  "isClinical": "no",
+  "isIonising": "no",
+  "isBioSample": "no",
+  "participantConsent": "yes",
+  "isCapable": "yes",
+  "noConsent": "no",
+  "isHMPPS": "no",
+  "hmppsNations": "England",
+  "isMOD": "no",
+  "isHFEA": "no",
+
+  // Autocomplete (needs the stored value, not display text)
+  "sponsorOrg": "uOfMan"
 }
